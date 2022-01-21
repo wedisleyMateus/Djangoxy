16f45be6e6292251901534c7fa00cf76a5a01c7a
wedisleyMateus <wedmat282016@gmail.com>
2022-01-21 19:12:12 -0300

    Changed the HTML for the site

--- WM_Corporation/settings.py
+++ WM_Corporation/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-81wjb^ok+bd5f8&o23-c!@u#8o4^cf81hpxb$sqv#(&pgjcksd
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
--- content_era/views.py
+++ content_era/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def content_list(request):
+    return render(request, 'content/content_list.html', {})
